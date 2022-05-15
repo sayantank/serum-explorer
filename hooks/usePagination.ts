abac8347de44c60b276529a599f3115c06455a31
Sayantan Karmakar <sayantankarmakar@outlook.com>
2022-05-15 08:04:05 +0530

    pagination setup

---
+++ hooks/usePagination.ts
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+export const usePagination = <T>(
+  data: Array<T> | undefined,
+  pageSize: number
+) => {
+  const [pageData, setPageData] = useState<Array<T> | undefined>(undefined);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    if (data) {
+      const startIndex = pageNumber * pageSize - pageSize;
+      setPageData(data.splice(startIndex, pageSize));
+      setTotalPages(Math.ceil(data.length / pageSize));
+    }
+  }, [data, pageNumber, pageSize]);
+
+  useEffect(() => {
+    setPageNumber(1);
+  }, [data]);
+
+  const nextPage = () => {
+    setPageNumber(pageNumber + 1);
+  };
+
+  const prevPage = () => {
+    if (pageNumber === 1) return;
+    setPageNumber(pageNumber - 1);
+  };
+
+  if (!data) {
+    return {
+      pageData: undefined,
+      pageNumber: 0,
+      totalPages: 0,
+      nextPage: undefined,
+      prevPage: undefined,
+    };
+  }
+
+  return {
+    pageData,
+    pageNumber,
+    totalPages,
+    nextPage,
+    prevPage,
+  };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,39 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import { PaginationButton } from "../components/common/Pagination/PaginationButton";
 import { getLayout } from "../components/layouts/SiteLayout";
+import { usePagination } from "../hooks/usePagination";
 import { SerumMarketInfo, useSerumMarkets } from "../hooks/useSerumMarkets";
 
 const Home = () => {
   const router = useRouter();
   const { serumMarkets, loading } = useSerumMarkets();
+
+  const [filterString, setFilterString] = useState("");
+  const [filteredMarkets, setFilteredMarkets] = useState(serumMarkets);
+
+  const {
+    pageData: pageMarkets,
+    totalPages,
+    pageNumber,
+    prevPage,
+    nextPage,
+  } = usePagination(filteredMarkets, 5);
+
+  useEffect(() => {
+    if (serumMarkets) {
+      const q = new RegExp(filterString, "i");
+      setFilteredMarkets(
+        serumMarkets.filter(
+          (row) =>
+            row.address.toString().match(q) ||
+            row.baseSymbol?.match(q) ||
+            row.quoteSymbol?.match(q)
+        )
+      );
+    }
+  }, [filterString, serumMarkets]);
 
   useEffect(() => {
     console.log(router.query);
@@ -14,17 +41,17 @@ const Home = () => {
 
   const MarketListItem = ({ market }: { market: SerumMarketInfo }) => {
     return (
-      <div className="bg-cyan-800 hover:bg-cyan-700 transition-colors p-4 rounded flex items-center justify-between">
+      <div className="bg-cyan-800 hover:bg-cyan-700 transition-colors py-2 px-4 rounded flex items-center justify-between">
         <div className="flex flex-col">
           <h3 className="text-xs text-cyan-400">Address</h3>
-          <p className="font-medium">
+          <p className="font-medium text-sm ">
             {market.address.toString().slice(0, 12)}...
           </p>
         </div>
         {market.baseSymbol && market.quoteSymbol ? (
           <div className="flex flex-col items-end">
             <h3 className="text-xs text-cyan-400">Tokens</h3>
-            <p className="font-medium">
+            <p className="font-medium text-sm">
               {market.baseSymbol}/{market.quoteSymbol}
             </p>
           </div>
@@ -38,24 +65,43 @@ const Home = () => {
       {/* serumMarkets is truthy only on devnet/localnet */}
       {serumMarkets ? (
         <div className="flex flex-col space-y-2">
-          <h2 className="text-2xl font-semibold">Markets</h2>
-          <ul className="flex flex-col space-y-3">
-            {serumMarkets?.map((market) => (
-              <li key={market.address.toString()} className="cursor-pointer">
-                <Link
-                  passHref
-                  href={{
-                    pathname: `/market/${market.address.toString()}`,
-                    query: router.query,
-                  }}
-                >
-                  <a>
-                    <MarketListItem market={market} />
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
+          {pageMarkets ? (
+            <div className="bg-cyan-700 p-4 rounded flex flex-col space-y-4">
+              <h2 className="text-2xl font-semibold">Markets</h2>
+              <input
+                type="text"
+                value={filterString}
+                onChange={(e) => setFilterString(e.target.value)}
+                placeholder="Search by address or token symbols"
+                className="w-full p-2 rounded border border-cyan-300 bg-transparent focus:outline-none text-sm"
+              />
+              <ul className="flex flex-col space-y-3">
+                {pageMarkets.map((market) => (
+                  <li
+                    key={market.address.toString()}
+                    className="cursor-pointer"
+                  >
+                    <Link
+                      passHref
+                      href={{
+                        pathname: `/market/${market.address.toString()}`,
+                        query: router.query,
+                      }}
+                    >
+                      <a>
+                        <MarketListItem market={market} />
+                      </a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <div className="flex items-center justify-between">
+                <PaginationButton type="prev" onClick={prevPage} />
+                <p>{`${pageNumber}/${totalPages}`}</p>
+                <PaginationButton type="next" onClick={nextPage} />
+              </div>
+            </div>
+          ) : null}
         </div>
       ) : null}
     </div>
