e6b530dcf6e7162e481e5105d171aa42fbdc5e8e
Sayantan Karmakar <sayantankarmakar@outlook.com>
2022-05-16 18:34:45 +0530

    fix header router.query bug

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
 import { FC } from "react";
 import { useSolana } from "../../context/SolanaContext";
 import { SettingsButton } from "./SettingsButton";
@@ -11,18 +12,23 @@ const Header: FC<HeaderProps> = () => {
   const router = useRouter();
   const { cluster } = useSolana();
 
+  const sanitizeQuery = (query: ParsedUrlQuery) => {
+    if (query.address) {
+      delete query.address;
+    }
+    return { ...query };
+  };
+
   return (
     <div className="w-full py-4 px-4 md:px-0 flex items-center justify-between border-b-2 border-b-cyan-900 mb-4">
-      <Link
-        href={{
-          pathname: "/",
-          query: router.query,
-        }}
+      <button
+        className="font-bold text-2xl text-white no-underline"
+        onClick={() =>
+          router.push({ pathname: "/", query: sanitizeQuery(router.query) })
+        }
       >
-        <a className="font-bold text-2xl text-white no-underline">
-          Serum Explorer
-        </a>
-      </Link>
+        Serum Explorer
+      </button>
       <div className="flex items-center space-x-4">
         <p className="text-sm text-cyan-500 text-right">{cluster.label}</p>
         <SettingsButton />
